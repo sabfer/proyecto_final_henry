c66776630cf41077d299552b1ecc46dab26d79ea
Federico <f.gabrielvillalba@gmail.com>
2021-09-28 19:01:52 -0300

    cambio de formato fecha en order

--- api/src/models/Orders.js
+++ api/src/models/Orders.js
@@ -1,16 +1,15 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
-var Client = mongoose.model('Client');
-var User = mongoose.model('User');
-var Product = mongoose.model('Product');
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var Client = mongoose.model("Client");
+var User = mongoose.model("User");
+var Product = mongoose.model("Product");
 
 var orders = new Schema({
-    date: String,
-    hour: String,
-    products: [{ type: Schema.ObjectId, ref: "Product" }],
-    clientId: { type: Schema.ObjectId, ref: "Client" },
-    userId: { type: Schema.ObjectId, ref: "User"},
-    typeOrder : { type: String, enum : ['Take Away', 'Delivery', 'In Place']},
-})
+  date: Date,
+  products: [{ type: Schema.ObjectId, ref: "Product" }],
+  clientId: { type: Schema.ObjectId, ref: "Client" },
+  userId: { type: Schema.ObjectId, ref: "User" },
+  typeOrder: { type: String, enum: ["Take Away", "Delivery", "In Place"] },
+});
 
-module.exports = mongoose.model('Order', orders)
+module.exports = mongoose.model("Order", orders);
--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const mongoose = require('mongoose');
-const router = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
 const Orders = require("../models/Orders");
 const User = require("../models/Users");
@@ -8,33 +8,30 @@ const Product = require("../models/Products");
 const Client = require("../models/Clients");
 
 router.get("/", async function (req, res) {
-    Orders.find({}, function (err, orders) {
-        User.populate(orders, {path: 'orders'}, function (err, orders) {
-            res.status(200).send(orders);
-        })
-    })
-}); 
-
+  Orders.find({}, function (err, orders) {
+    User.populate(orders, { path: "orders" }, function (err, orders) {
+      res.status(200).send(orders);
+    });
+  });
+});
 
 router.post("/", async function (req, res) {
+  const { products, clientId, userId, typeOrder } = req.body;
 
-    const {date, hour, products, clientId, userId, typeOrder} = req.body;
+  const product = await Product.find({ type: products });
+  const client = await Client.findById(clientId);
+  const user = await User.findById(userId);
 
-    const product = await Product.find({type: products});
-    const client = await Client.findById(clientId);
-    const user = await User.findById(userId);
+  const order = await new Orders({
+    date: new Date(),
+    products: product,
+    clientId: client,
+    userId: user,
+    typeOrder,
+  });
 
-    const order = await new Orders({
-        date,
-        hour,
-        products: product,
-        clientId: client,
-        userId: user,
-        typeOrder,
-    })
-
-    await order.save();
-    res.send(order);
-})
+  await order.save();
+  res.send(order);
+});
 
 module.exports = router;
