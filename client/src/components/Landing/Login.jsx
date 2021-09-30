558394afbc381e485818d720cf80d0dab2ff7272
Sabino Fernandez <sabinofernandezr@gmail.com>
2021-09-30 08:39:39 -0500

    modifyng layout

--- client/src/components/Landing/Login.jsx
+++ client/src/components/Landing/Login.jsx
@@ -30,48 +30,43 @@ function Login() {
         </nav>
       </header>
 
-      <section className={style.login_form}>
-        <div className={style.login_top}>
-          <h2>Bienvenido</h2>
-          <div className={style.overlay}></div>
-        </div>
-        <div className={style.login_bottom}>
-          <div className={style.form}>
-            <h2 className={style.title}>Iniciar sesión</h2>
-            <div className={style.input_container}>
-              <input
-                required
-                id="email"
-                className={style.input}
-                type="email"
-                placeholder=" "
-                autoComplete="off"
-              />
-              <label htmlFor="email" className={style.placeholder}>
-                Correo electrónico
-              </label>
-            </div>
-            <div className={style.input_container}>
-              <input
-                required
-                id="password"
-                className={style.input}
-                type="password"
-                placeholder=" "
-                autoComplete="off"
-              />
-              <label htmlFor="password" className={style.placeholder}>
-                Contraseña
-              </label>
-            </div>
-            <div className={style.signUpContainer}>
-              <button type="submit" className={style.submit}>
-                Ingresar
-              </button>
-              <div id="gSignInWrapper" className={style.gSignInWrapper}>
-                <div id="customBtn" className={style.customGPlusSignIn}>
-                  <div className={style.icon}></div>
-                  <span className={style.buttonText}>Continuar con Google</span>
+      <section className={style.login_container}>
+        <div className={style.login_form}>
+          <div className={style.login_top}>
+            <h2>Bienvenido</h2>
+            <div className={style.overlay}></div>
+          </div>
+          <div className={style.login_bottom}>
+            <div className={style.form}>
+              <h2 className={style.title}>Iniciar sesión</h2>
+              <div className={style.input_container}>
+                <input required id="email" className={style.input} type="email" placeholder=" " autoComplete="off" />
+                <label htmlFor="email" className={style.placeholder}>
+                  Correo electrónico
+                </label>
+              </div>
+              <div className={style.input_container}>
+                <input
+                  required
+                  id="password"
+                  className={style.input}
+                  type="password"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label htmlFor="password" className={style.placeholder}>
+                  Contraseña
+                </label>
+              </div>
+              <div className={style.signUpContainer}>
+                <button type="submit" className={style.submit}>
+                  Ingresar
+                </button>
+                <div id="gSignInWrapper" className={style.gSignInWrapper}>
+                  <div id="customBtn" className={style.customGPlusSignIn}>
+                    <div className={style.icon}></div>
+                    <span className={style.buttonText}>Continuar con Google</span>
+                  </div>
                 </div>
               </div>
             </div>
--- client/src/components/Landing/RegisterPage.jsx
+++ client/src/components/Landing/RegisterPage.jsx
@@ -30,63 +30,58 @@ function Register() {
         </nav>
       </header>
 
-      <section className={style.register_form}>
-        <div className={style.register_top}>
-          <h2>
-            Registrate y conoce las <br /> bondades de una buena <br /> gestión
-          </h2>
-          <div className={style.overlay}></div>
-        </div>
-        <div className={style.register_bottom}>
-          <div className={style.form}>
-            <h2 className={style.title}>Registro</h2>
-            <div className={style.input_container}>
-              <input
-                required
-                id="email"
-                className={style.input}
-                type="email"
-                placeholder=" "
-                autoComplete="off"
-              />
-              <label htmlFor="email" className={style.placeholder}>
-                Correo electrónico
-              </label>
-            </div>
-            <div className={style.input_container}>
-              <input
-                required
-                id="password"
-                className={style.input}
-                type="password"
-                placeholder=" "
-                autoComplete="off"
-              />
-              <label htmlFor="password" className={style.placeholder}>
-                Contraseña
-              </label>
-            </div>
-            <div className={style.input_container}>
-              <input
-                required
-                id="confirmPassword"
-                className={style.input}
-                type="password"
-                placeholder=" "
-                autoComplete="off"
-              />
-              <label htmlFor="confirmPassword" className={style.placeholder}>
-                Confirmar contraseña
-              </label>
-            </div>
-            <div className={style.signUpContainer}>
-              <button type="submit" className={style.registerSubmit}>
-                Registrarme
-              </button>
-              <div id="gSignInWrapper" className={style.gSignInWrapper}>
-                <div id="customBtn" className={style.customGPlusSignIn}>
-                  <div className={style.icon}></div>
-                  <span className={style.buttonText}>Continuar con Google</span>
+      <section className={style.register_container}>
+        <div className={style.register_form}>
+          <div className={style.register_top}>
+            <h2>
+              Registrate y conoce las <br /> bondades de una buena <br /> gestión
+            </h2>
+            <div className={style.overlay}></div>
+          </div>
+          <div className={style.register_bottom}>
+            <div className={style.form}>
+              <h2 className={style.title}>Registro</h2>
+              <div className={style.input_container}>
+                <input required id="email" className={style.input} type="email" placeholder=" " autoComplete="off" />
+                <label htmlFor="email" className={style.placeholder}>
+                  Correo electrónico
+                </label>
+              </div>
+              <div className={style.input_container}>
+                <input
+                  required
+                  id="password"
+                  className={style.input}
+                  type="password"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label htmlFor="password" className={style.placeholder}>
+                  Contraseña
+                </label>
+              </div>
+              <div className={style.input_container}>
+                <input
+                  required
+                  id="confirmPassword"
+                  className={style.input}
+                  type="password"
+                  placeholder=" "
+                  autoComplete="off"
+                />
+                <label htmlFor="confirmPassword" className={style.placeholder}>
+                  Confirmar contraseña
+                </label>
+              </div>
+              <div className={style.signUpContainer}>
+                <button type="submit" className={style.registerSubmit}>
+                  Registrarme
+                </button>
+                <div id="gSignInWrapper" className={style.gSignInWrapper}>
+                  <div id="customBtn" className={style.customGPlusSignIn}>
+                    <div className={style.icon}></div>
+                    <span className={style.buttonText}>Continuar con Google</span>
+                  </div>
                 </div>
               </div>
             </div>
