a39802ab5ebe90e3b387ef8b15601b537fa59cbe
Enzo Derviche <enzoderviche00@gmail.com>
2021-09-29 22:58:03 -0300

    finishCrud/Clients/products

---
+++ api/src/controllers/clients.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Clients = require("../models/Clients");
+
+// GETS
+const clientByName = async(name) => {
+    const client = await Clients.findOne({ name: `${name}`});
+    return client ? client : null;
+}
+
+const searchClients = async() => {
+    const clients = await Clients.find();
+    return clients ? clients : null;
+}
+
+// POST
+const filterClient = async(name) => {
+    const clients = await Clients.findOne({ name: `${name}`});
+    return clients ? true : null;
+}
+
+const addClient = async(name, phone, direction) => {
+    const newClient = await new Clients({
+        name, 
+        phone,
+        direction
+    })
+    await newClient.save();
+    return newClient;
+}
+
+// DELETE
+const deleteClient = async(id) => {
+    const deleted = await Clients.deleteOne({_id: `${id}`});
+    return deleted ? true : false;
+}
+
+// UPDATE/PUT
+const updateClient = async(id, update) => {
+    const updated = await Clients.findOneAndUpdate({_id: id} , update, {new:true})
+    return updated ? true : false;
+}
+
+module.exports = {
+    clientByName,
+    searchClients,
+    filterClient,
+    addClient,
+    deleteClient,
+    updateClient
+};
--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,55 +1,69 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const router = express.Router();
-
-const Products = require("../models/Products");
-
-
+const {
+    searchProduct, 
+    searchProducts,
+    filterProduct,
+    addProduct,
+    deleteProduct,
+    updateProduct
+} = require("../controllers/products.js");
 
 router.get("/", async function (req, res) {
-
     const {name} = req.query;
-
-    if (name){
-        try{
-            const product = await Products.findOne({ name:name });
-            product ? res.send(product)
-            : res.status(404).send("Product not exists");
-        } catch(err){
-            console.error(err);
-        }
-    }
-    else {
-        try {
-            const listProducts = await Products.find();
-            listProducts ? res.send(listProducts)
-            : res.status(404).send("Products not exists");
-        } catch (err) {
-            console.error(err);
-        }
+    try{
+        if (name){
+                const product = await searchProduct(name);
+                product ? res.send(product)
+                : res.status(404).send("Product not exists!");
+        } else {
+                const listProducts = await searchProducts();
+                listProducts ? res.send(listProducts)
+                : res.status(404).send("Products not exists");
+        } 
+    } catch (err) {
+        console.error(err);
     }
 }); 
 
 router.post("/", async function (req, res) {
     const {price, name} = req.body;
     try{
-        const searchProduct = await Products.findOne({name:name});
+        const newProduct = await filterProduct(name);
 
-        if(searchProduct){
+        if(newProduct){
             res.status(400).send("Product already exists") 
         }  else {
-            const product = await new Products({
-                price,
-                name,
-            })
-            await product.save();
+            const product = await addProduct(price, name);
             res.send(product);    
         }
-        
     } catch (err) {
         console.log(err);
     }
 })
 
+router.delete("/:id", async function(req, res) {
+    const {id} = req.params;
+    try{
+        const eliminated = await deleteProduct(id);
+        eliminated ? res.send('Product deleted successfully!')
+        : null; 
+    } catch (err) {
+        res.status(400).send("Product not exists");
+    }
+})
+
+router.put("/:id", async function(req, res) {
+    const {id} = req.params;
+    const productsUpdate = req.body;
+
+    try {
+        const update = await updateProduct(id, productsUpdate)
+        update ? res.send('Product updated successfully!')
+        : null; 
+    } catch (err) {
+        res.status(400).send("Product not exist") 
+    }
+}) 
 
 module.exports = router;
