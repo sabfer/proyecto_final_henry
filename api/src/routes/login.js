2de02844dd3891e6ec459cc46ddc439191aa6a75
Federico <f.gabrielvillalba@gmail.com>
2021-10-01 15:01:24 -0300

    ComerceType modelo/rutas(CRUD)

---
+++ api/src/routes/login.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  const { userEmail, userPassword } = req.body;
+  try {
+    const usuario = await Users.findOne({ email: userEmail });
+
+    console.log(
+      "holaaaa:",
+      await bcrypt.compare(userPassword, usuario.password)
+    );
+    res.json(usuario);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+module.exports = router;
