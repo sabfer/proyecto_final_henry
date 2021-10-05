4eef1c8536deb5b70129e3b580a5763855cedd40
Enzo Derviche <enzoderviche00@gmail.com>
2021-10-05 17:01:15 -0300

    deleteVars

--- api/src/models/Clients.js
+++ api/src/models/Clients.js
@@ -1,7 +1,7 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var clients = new Schema({
+const clients = new Schema({
   name: { type: String, required: true },
   phone: { type: Number, required: true },
   direction: { type: String, required: true },
