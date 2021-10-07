faa3086237e43dc5f3806916e07aa801bfa5f91a
Alevalverde <alevalverdev8@hotmail.com>
2021-10-07 09:58:35 -0300

    all

--- client/src/components/Settings/components/NumberOfProducts.js
+++ client/src/components/Settings/components/NumberOfProducts.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 export default function NumberOfProducts() {
   const products = useSelector((state) => state.products);
+  console.log("aguante boca")
   return products && products.length >= 1 ? (
     <div>{products.length} productos cargados exitosamente</div>
   ) : (
