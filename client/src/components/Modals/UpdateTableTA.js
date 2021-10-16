e3dc668fbdc30e47043074e66b52114cad7ef100
Sabino Fernandez <sabinofernandez0@gmail.com>
2021-10-15 21:10:15 -0500

    fix

--- client/src/components/Modals/UpdateTableTA.js
+++ client/src/components/Modals/UpdateTableTA.js
@@ -1,14 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateOrder, getTakeAwayOrders } from "../../actions/index";
-import {
-    Table,
-    TableHead,
-    TableData,
-    TableHd,
-    TableRow,
-    Options
-} from "../../css/Table";
+import { Table, TableHead, TableData, TableHd, TableRow, Options } from "../../css/Table";
 import { Button } from "../../css";
 import { Select } from "../../css/Select";
 import {
@@ -232,141 +225,46 @@ export default function UptadeTableTA({ state, setStateModal, orderNumber }) {
           icon: "success",
           confirmButtonColor: "#00A0D2",
         });
-    return (
-        <div>
-        {order && (
-            <Overlay display={state ? "flex" : "none"}>
-            <ModalContainer>
-                <HeaderModal>
-                <img src="https://i.imgur.com/ZyWpO0s.png" alt="img not found" />
-                <HeaderModalTitle>
-                    <h3>Orden: {ordenActual.orderNumber} </h3>
-                    <h4>Cliente: {order.nameClient} </h4>
-                </HeaderModalTitle>
-                <HeaderModalDetails>
-                    <p>Hora de pedido: {ordenActual.hour}</p>
-                </HeaderModalDetails>
-                </HeaderModal>
-                <CloseButton onClick={(e) => handleClose(e)}>
-                <FontAwesomeIcon icon={faWindowClose} />
-                </CloseButton>
-                <OrderContainer>
-                <div>
-                    <CategoriasPedidos>
-                    <FilterProductTypes />
-                    </CategoriasPedidos>
+        setStateModal(!state);
+      }
+    });
+  }
+  return (
+    <div>
+      {order && (
+        <Overlay display={state ? "flex" : "none"}>
+          <ModalContainer>
+            <HeaderModal>
+              <img src="https://i.imgur.com/ZyWpO0s.png" alt="img not found" />
+              <HeaderModalTitle>
+                <h3>Orden: {ordenActual.orderNumber} </h3>
+                <h4>Cliente: {order.nameClient} </h4>
+              </HeaderModalTitle>
+              <HeaderModalDetails>
+                <p>Hora de pedido: {ordenActual.hour}</p>
+              </HeaderModalDetails>
+            </HeaderModal>
+            <CloseButton onClick={(e) => handleClose(e)}>
+              <FontAwesomeIcon icon={faWindowClose} />
+            </CloseButton>
+            <OrderContainer>
+              <div>
+                <CategoriasPedidos>
+                  <FilterProductTypes />
+                </CategoriasPedidos>
 
-                    <SelectModal>
-                    <FormModal onSubmit={(e) => handleSubmitAddProduct(e)}>
-                        <Select
-                        id="selectProduct"
-                        width="83%"
-                        height="2.4rem"
-                        border="solid 1px black"
-                        fontWeight="bold"
-                        onChange={(e) => handleChangeProduct(e)}
-                        name="name"
-                        >
-                        <option
-                            id="inputDefault"
-                            value="none"
-                            selected
-                            disabled
-                            hidden
-                        >
-                            Seleccione un producto
-                        </option>
-                        {products &&
-                            products.map((e) => {
-                            return (
-                                <option key={e._id} value={e.name}>
-                                {" "}
-                                {e.name}{" "}
-                                </option>
-                            );
-                            })}
-                        </Select>
-                        <InputModal>
-                        <input
-                            type="number"
-                            placeholder="Cant."
-                            onChange={(e) => handleChangeProduct(e)}
-                            name="amount"
-                            value={producto.amount}
-                        />
-                        </InputModal>
-                        <Button type="submit" width="8%" buttonColor="#00C72C">
-                        <FontAwesomeIcon icon={faCheck} />
-                        </Button>
-                    </FormModal>
-                    </SelectModal>
-                    <Table id="productsTable">
-                    <TableHead>
-                        <TableRow>
-                        <TableHd width="45%">
-                            <span className="productName">
-                            <p style={{ margin: 0 }}>Nombre</p>
-                            </span>
-                        </TableHd>
-                        <TableHd width="15%">Precio</TableHd>
-                        <TableHd width="20%">Cantidad</TableHd>
-                        <TableHd width="20%">Option</TableHd>
-                        </TableRow>
-                    </TableHead>
-                    <tbody>
-                        {ordenActual &&
-                        ordenActual.products.map((product) => {
-                            return (
-                            <TableRow key={product.name}>
-                                <TableData>{product.name}</TableData>
-                                <TableData>{product.price}</TableData>
-                                <TableData>
-                                <input
-                                    onChange={(e) =>
-                                    handleInputAmount(e, product.name)
-                                    }
-                                    placeholder={product.amount}
-                                />
-                                </TableData>
-                                <TableData>
-                                <Options justify="center">
-                                    <Button
-                                    onClick={(e) =>
-                                        handleDelete(product.name)
-                                    }
-                                    width="2rem"
-                                    height="2rem"
-                                    buttonColor="rgba(255, 0, 0, 1)"
-                                    >
-                                    <FontAwesomeIcon
-                                        icon={faTrash}
-                                    ></FontAwesomeIcon>
-                                    </Button>
-                                </Options>
-                                </TableData>
-                            </TableRow>
-                            );
-                        })}
-                    </tbody>
-                    </Table>
-                </div>
-                <div style={{ display: "flex" }}>
-                    <TablePricesModal>
-                    <PaymentCheckBox handlePaymentInput={handlePaymentInput} />
-                    <p>Monto Total: ${ordenActual.totalPrice}</p>
-                    <Button
-                        width="8rem"
-                        height="25px"
-                        buttonColor="#00C2FF"
-                        onClick={() =>
-                        handleCloseOrder(ordenActual.id, {
-                            products: ordenActual.products,
-                            totalPrice: ordenActual.totalPrice,
-                            estado: "Finalizada",
-                        })
-}
+                <SelectModal>
+                  <FormModal onSubmit={(e) => handleSubmitAddProduct(e)}>
+                    <Select
+                      id="selectProduct"
+                      width="83%"
+                      height="2.4rem"
+                      border="solid 1px black"
+                      fontWeight="bold"
+                      onChange={(e) => handleChangeProduct(e)}
+                      name="name"
                     >
-                      <option id="inputDefault" value="none" defaultValue disabled hidden>
+                      <option id="inputDefault" value="none" selected disabled hidden>
                         Seleccione un producto
                       </option>
                       {products &&
