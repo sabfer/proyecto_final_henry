68442ee1da14b5a3fc841a9accdf5f9fb0e54b2c
Enzo Derviche <enzoderviche00@gmail.com>
2021-10-21 17:53:54 -0300

    inventario

--- client/src/components/Settings/views/Informes/components/Views/Inventario.js
+++ client/src/components/Settings/views/Informes/components/Views/Inventario.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getProductsInv,
   deleteProductInv,
+  getProductsInv,
   orderTheProductsInv,
 } from "../../../../../../actions";
 //------------------------------------------\\
